aba0433615679cb5de76b81d4174fdee65f56354
Rob Meade <baldy.meade@gmail.com>
2018-08-23 00:07:04 +0100

    Project initial commit

---
+++ Assets/Scripts/ShowStats.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class ShowStats : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.name + " inertia tensor : " + rigidBody.inertiaTensor);
+        Debug.Log(gameObject.name + " - Center of Mass : " + rigidBody.worldCenterOfMass);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(rigidBody.worldCenterOfMass, 0.2f);
+    }
+}
---
+++ Assets/Scripts/Torque.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Torque : MonoBehaviour
+{
+    public Vector3 torque;
+    public float torqueTime;
+
+    private Rigidbody rigidBody;
+
+
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        if(torqueTime >= 0f)
+        {
+            rigidBody.AddTorque(torque);
+
+            torqueTime -= Time.deltaTime;
+        }
+    }
+}
